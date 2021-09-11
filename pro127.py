d12b22f85a415afd3d1bbbd1fc3cff7b584fe323
SaranGowrishVelisetty99 <67533087+SaranGowrishVelisetty99@users.noreply.github.com>
2021-09-11 15:57:02 +0530

    Add files via upload

---
+++ pro127.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import csv
+
+START_URL = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+browser = webdriver.Chrome("C:/Users/hp/Downloads/chromedriver_win32/chromedriver")
+browser.get(START_URL)
+time.sleep(10)
+def scrape():
+    headers = ["name", "light_years_from_earth", "planet_mass", "stellar_magnitude", "discovery_date"]
+    planet_data = []
+    for i in range(0, 452):
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+        for ul_tag in soup.find_all("table", attrs={"class", "wikitable sortable jquery-tablesorter"}):
+            li_tags = ul_tag.find_all("tr")
+            temp_list = []
+            for index, li_tag in enumerate(li_tags):
+                if index == 0:
+                    temp_list.append(li_tag.find_all("a")[0].contents[0])
+                    print("it is being scraped!!")
+                else:
+                    try:
+                        temp_list.append(li_tag.contents[0])
+                    except:
+                        temp_list.append("")
+            planet_data.append(temp_list)
+    with open("scrapper_2.csv", "w") as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(headers)
+        csvwriter.writerows(planet_data)
+scrape()
